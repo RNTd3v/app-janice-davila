93adabf8d01fd3c4b0f0d84417ca4b69dfc425ed
Renato Andrade <renato@rntdesign.com.br>
2018-10-20 23:03:28 -0300

    add https nas chamda das apis

--- public/env-config.js
+++ public/env-config.js
@@ -1,5 +1,5 @@
 const prod = process.env.NODE_ENV === 'production'
 module.exports = {
-  'process.env.API_URL': prod ? 'http://142.93.204.233/api/v1' : 'http://127.0.0.1:3333/api/v1',
-  'process.env.URL': prod ? 'http://142.93.204.233/' : 'http://127.0.0.1:3333/'
+  'process.env.API_URL': prod ? 'https://janicedavila.com/api/v1' : 'http://127.0.0.1:3333/api/v1',
+  'process.env.URL': prod ? 'https://janicedavila.com/' : 'http://127.0.0.1:3333/'
 }
