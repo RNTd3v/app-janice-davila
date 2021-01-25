11fb66d60e03ff7395100084abed0929d4f957cb
Renato de Andrade Soares <renato.andrade@opus-software.com.br>
2021-01-25 10:45:13 -0300

    Ajustes do player modal no filmes.js

--- public/pages/pt/filmes.js
+++ public/pages/pt/filmes.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { isMobile, isBrowser } from "react-device-detect";
 import axios from 'axios';
 
 import Head from 'next/head';
-import Router from 'next/router'
 
 import withAnalytics from '../../src/hocs/withAnalytics';
 
@@ -11,11 +11,57 @@ import withAnalytics from '../../src/hocs/withAnalytics';
 import Logo from '../../src/components/logo';
 import Menu from '../../src/components/menu';
 import Language from '../../src/components/language';
+import ModalVimeo from '../../src/components/modal-vimeo';
 import Footer from '../../src/components/footer';
+class Filmes extends React.Component {
 
-const Filmes = ({ categories }) => (
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+      showModalVimeo: false,
+      selectedFilm: {}
+    }
+
+    this.hideModalVimeo = this.hideModalVimeo.bind(this);
+  }
+
+  componentDidMount() {
+    this.getFilms();
+  }
+
+  async getFilms() {
+    const { data } = await axios.get(
+      `${process.env.API_URL}/category/films/videos`
+    );
+    this.setState({categories: data});
+  }
+
+  selectVideo(film) {
+    const selectedFilm = {
+      ...film,
+      vimeo_id: film.videos[0].vimeo_id
+    }
+    this.setState({ selectedFilm, showModalVimeo: true });
+  }
+
+  hideModalVimeo() {
+    this.setState({ selectedFilm: null, showModalVimeo: false });
+  }
+
+  handleLazyLoadingImage(index) {
+    if (isBrowser && index < 3) {
+      return 'eager'
+    }
+    return 'lazy';
+  }
+
+  render() {
+  const { categories, showModalVimeo } = this.state;
+  return (
     <div>
         <Head>
+            <html lang="en-US" />
             <title>Janice d'Avila - Cinematographer</title>
         </Head>
         <section>
@@ -41,31 +87,34 @@ const Filmes = ({ categories }) => (
                                     {
                                         category.films
                                         .sort((a, b) => a.order_by - b.order_by)
-                                        .map(film => (
-                                            <article className="item" key={film.id} >
-                                                <img src={film.picture} className="picture" alt={film.title_pt} />
-                                                <span className="content">
-                                                    <h3 className="title">{film.title_pt}</h3>
-                                                    <small className="link" onClick={() => Router.push(`/detalhe/${film.id}`)}>ver mais</small>
-                                                </span>
-                                            </article>
+                                        .map((film, index) => (
+                                          <article className={'item ' + (isMobile ? '-mobile' : '')} key={film.id} onClick={() => this.selectVideo(film)}>
+                                            <img src={film.picture} className="picture" alt={film.title} loading={this.handleLazyLoadingImage(index)} />
+                                            <span className="content">
+                                              <h3 className="title">{film.title}</h3>
+                                              <p className="description">{film.description}</p>
+                                              <i className="fas fa-play-circle player"></i>
+                                            </span>
+                                          </article>
                                         ))
                                     }
                                 </section>
                             ))
                         }
+                        {
+                        showModalVimeo
+                        ? <div className="modal-container">
+                            <ModalVimeo film={this.state.selectedFilm} hideModalVimeo={this.hideModalVimeo} />
+                          </div>
+                        : null
+                      }
             </main>
             <Footer />
         </section>
 
     </div>
-);
-
-Filmes.getInitialProps = async () => {
-    const response = await axios.get(
-        `${process.env.API_URL}/category/films`
     );
-    return { categories: response.data }
+  }
 }
 
 export default withAnalytics()(Filmes);
