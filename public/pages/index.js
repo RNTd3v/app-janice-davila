c1c0813f37c3b2b856082dca393082eef8bcd94c
RNTOpus <100876139+RNTOpus@users.noreply.github.com>
2022-07-29 07:34:23 -0300

    fix mobile anchor name

--- public/pages/index.js
+++ public/pages/index.js
@@ -35,7 +35,7 @@ class Films extends React.Component {
     const { data } = await axios.get(
       `${process.env.API_URL}/category/films/videos`
     );
-    this.setState({categories: data});
+    this.setState({ categories: data });
   }
 
   selectVideo(film) {
@@ -62,59 +62,59 @@ class Films extends React.Component {
     return (
       <div>
         <Head>
-            <html lang="en-US" />
-            <title>Janice d'Avila - Cinematographer</title>
+          <html lang="en-US" />
+          <title>Janice d'Avila - Cinematographer</title>
         </Head>
         <section>
-            <header className="header">
-                <div className="container">
-                    <Logo />
-                    <div className="nav -wrapper">
-                        <Menu language="en" />
-                        <Language pt="/pt/filmes" en="/" active="en" />
-                    </div>
-                </div>
-                <div className="show-mobile">
-                    <AnchorLink offset='220' href='#Documentary'>Documentary</AnchorLink>
-                    <AnchorLink offset='220' href='#Series'>Series</AnchorLink>
-                    <AnchorLink offset='220' href='#Commercial'>Commercial</AnchorLink>
-                </div>
-            </header>
-            <main className="films container">
-                {
-                    categories.map(category => (
-                        <section className="film narrative" id={category.name} key={category.id}>
-                          <h2 className="title">{category.name}</h2>
-                            {
-                                category.films
-                                .sort((a, b) => a.order_by - b.order_by)
-                                .map((film, index) => (
-                                  <article className={'item ' + (isMobile ? '-mobile' : '')} key={film.id} onClick={() => this.selectVideo(film)}>
-                                      <img src={film.picture} className="picture" alt={film.title} loading={this.handleLazyLoadingImage(index)} />
-                                      <span className="content">
-                                        <h3 className="title">{film.title}</h3>
-                                        <p className="description">{film.description}</p>
-                                        <i className="fas fa-play-circle player"></i>
-                                      </span>
-                                    </article>
-                                ))
-                            }
+          <header className="header">
+            <div className="container">
+              <Logo />
+              <div className="nav -wrapper">
+                <Menu language="en" />
+                <Language pt="/pt/filmes" en="/" active="en" />
+              </div>
+            </div>
+            <div className="show-mobile">
+              <AnchorLink offset='220' href='#Documentary'>Documentary</AnchorLink>
+              <AnchorLink offset='220' href='#Narrative'>Narrative</AnchorLink>
+              <AnchorLink offset='220' href='#Commercial'>Commercial</AnchorLink>
+            </div>
+          </header>
+          <main className="films container">
+            {
+              categories.map(category => (
+                <section className="film narrative" id={category.name} key={category.id}>
+                  <h2 className="title">{category.name}</h2>
+                  {
+                    category.films
+                      .sort((a, b) => a.order_by - b.order_by)
+                      .map((film, index) => (
+                        <article className={'item ' + (isMobile ? '-mobile' : '')} key={film.id} onClick={() => this.selectVideo(film)}>
+                          <img src={film.picture} className="picture" alt={film.title} loading={this.handleLazyLoadingImage(index)} />
+                          <span className="content">
+                            <h3 className="title">{film.title}</h3>
+                            <p className="description">{film.description}</p>
+                            <i className="fas fa-play-circle player"></i>
+                          </span>
+                        </article>
+                      ))
+                  }
 
-                        </section>
-                    ))
-                }
-                {
-                  showModalVimeo
-                  ? <div className="modal-container">
-                      <ModalVimeo film={this.state.selectedFilm} hideModalVimeo={this.hideModalVimeo} />
-                    </div>
-                  : null
-                }
-            </main>
-            <Footer />
+                </section>
+              ))
+            }
+            {
+              showModalVimeo
+                ? <div className="modal-container">
+                  <ModalVimeo film={this.state.selectedFilm} hideModalVimeo={this.hideModalVimeo} />
+                </div>
+                : null
+            }
+          </main>
+          <Footer />
         </section>
 
-    </div>
+      </div>
     );
   }
 }
--- public/pages/pt/filmes.js
+++ public/pages/pt/filmes.js
@@ -34,7 +34,7 @@ class Filmes extends React.Component {
     const { data } = await axios.get(
       `${process.env.API_URL}/category/films/videos`
     );
-    this.setState({categories: data});
+    this.setState({ categories: data });
   }
 
   selectVideo(film) {
@@ -57,62 +57,62 @@ class Filmes extends React.Component {
   }
 
   render() {
-  const { categories, showModalVimeo } = this.state;
-  return (
-    <div>
+    const { categories, showModalVimeo } = this.state;
+    return (
+      <div>
         <Head>
-            <html lang="en-US" />
-            <title>Janice d'Avila - Cinematographer</title>
+          <html lang="en-US" />
+          <title>Janice d'Avila - Cinematographer</title>
         </Head>
         <section>
-            <header className="header">
-                <div className="container">
-                    <Logo />
-                    <div className="nav -wrapper">
-                        <Menu language="pt" />
-                        <Language pt="/pt/filmes" en="/" active="pt" />
-                    </div>
+          <header className="header">
+            <div className="container">
+              <Logo />
+              <div className="nav -wrapper">
+                <Menu language="pt" />
+                <Language pt="/pt/filmes" en="/" active="pt" />
+              </div>
+            </div>
+            <div className="show-mobile">
+              <AnchorLink offset='220' href='#Documentary'>Documentário</AnchorLink>
+              <AnchorLink offset='220' href='#Narrative'>Narrativa</AnchorLink>
+              <AnchorLink offset='220' href='#Commercial'>Comercial</AnchorLink>
+            </div>
+          </header>
+          <main className="films container">
+            {
+              categories.map(category => (
+                <section className="film narrative" id={category.name}>
+                  <h2 className="title">{category.name_pt}</h2>
+                  {
+                    category.films
+                      .sort((a, b) => a.order_by - b.order_by)
+                      .map((film, index) => (
+                        <article className={'item ' + (isMobile ? '-mobile' : '')} key={film.id} onClick={() => this.selectVideo(film)}>
+                          <img src={film.picture} className="picture" alt={film.title} loading={this.handleLazyLoadingImage(index)} />
+                          <span className="content">
+                            <h3 className="title">{film.title}</h3>
+                            <p className="description">{film.description}</p>
+                            <i className="fas fa-play-circle player"></i>
+                          </span>
+                        </article>
+                      ))
+                  }
+                </section>
+              ))
+            }
+            {
+              showModalVimeo
+                ? <div className="modal-container">
+                  <ModalVimeo film={this.state.selectedFilm} hideModalVimeo={this.hideModalVimeo} />
                 </div>
-                <div className="show-mobile">
-                    <AnchorLink offset='220' href='#Documentary'>Documentário</AnchorLink>
-                    <AnchorLink offset='220' href='#Series'>Series</AnchorLink>
-                    <AnchorLink offset='220' href='#Commercial'>Comercial</AnchorLink>
-                </div>
-            </header>
-            <main className="films container">
-                        {
-                            categories.map(category => (
-                                <section className="film narrative" id={category.name}>
-                                <h2 className="title">{category.name_pt}</h2>
-                                    {
-                                        category.films
-                                        .sort((a, b) => a.order_by - b.order_by)
-                                        .map((film, index) => (
-                                          <article className={'item ' + (isMobile ? '-mobile' : '')} key={film.id} onClick={() => this.selectVideo(film)}>
-                                            <img src={film.picture} className="picture" alt={film.title} loading={this.handleLazyLoadingImage(index)} />
-                                            <span className="content">
-                                              <h3 className="title">{film.title}</h3>
-                                              <p className="description">{film.description}</p>
-                                              <i className="fas fa-play-circle player"></i>
-                                            </span>
-                                          </article>
-                                        ))
-                                    }
-                                </section>
-                            ))
-                        }
-                        {
-                        showModalVimeo
-                        ? <div className="modal-container">
-                            <ModalVimeo film={this.state.selectedFilm} hideModalVimeo={this.hideModalVimeo} />
-                          </div>
-                        : null
-                      }
-            </main>
-            <Footer />
+                : null
+            }
+          </main>
+          <Footer />
         </section>
 
-    </div>
+      </div>
     );
   }
 }
